868121b3f288768a642ad1dd6238265c1e023ebd
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-07-10 22:29:10 -0400

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Todo from "./Todo";
+
+import { connect } from "react-redux";
+import { getTodosByVisibilityFilter } from "../redux/selectors"
+
+const TodoList = ({ todos }) => (
+  <ul className="todo-list">
+    {todos && todos.length
+      ? todos.map((todo, index) => {
+          return <Todo key={`todo-${todo.id}`} todo={todo} />;
+        })
+      : "No todos, yay!"}
+  </ul>
+);
+
+// calling the connect() function
+// call mapStateToProps as state => ({ todos: getTodos(state)})
+// call our export (TodoList)
+
+const mapStateToProps = state => {
+  const { visibilityFilter } = state;
+  const todos = getTodosByVisibilityFilter(state, visibilityFilter);
+  return { todos };
+}
+
+export default connect(mapStateToProps)(TodoList);
